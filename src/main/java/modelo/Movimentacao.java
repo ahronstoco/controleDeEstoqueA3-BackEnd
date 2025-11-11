c72139b05d17c6063a360a8f7dcc5b4f1cd1c97b
Ahron <AhronStoco@gmail.com>
2025-11-11 12:00:56 -0300

    feat: adicionado serialVersionUID na classe modelo Movimentacao.

--- src/main/java/modelo/Movimentacao.java
+++ src/main/java/modelo/Movimentacao.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Movimentacao implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private int idMovimentacao;
     private int idProduto;
